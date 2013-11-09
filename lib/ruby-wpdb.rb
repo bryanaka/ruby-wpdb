5a50cebdd6693a67a1ad3d346dbd4cfb20aedd57
Bryan M. Robles <bryanaka0@gmail.com>
2013-11-08 16:14:25 -0800

    first iteration of the wp-config.php parser

--- lib/ruby-wpdb.rb
+++ lib/ruby-wpdb.rb
@@ -12,7 +12,8 @@ module WPDB
     # config files found in that file.
     def from_config(file = nil)
       file ||= File.dirname(__FILE__) + '/../config.yml'
-      config = YAML::load_file(file)
+      ext = File.extname(file)
+      config = (ext == ".php" ? File.open(file) : YAML::load_file(file) )
 
       uri  = 'mysql2://'
       uri += "#{config['username']}:#{config['password']}"
@@ -45,6 +46,42 @@ module WPDB
       require_relative 'ruby-wpdb/links'
       require_relative 'ruby-wpdb/gravityforms'
     end
+
+    def from_wpconfig(file)
+      lines = [], table_prefix = nil
+      File.open(file).each_line do |line|
+        line.strip!
+        lines.push line if line.include? "define("
+        table_prefix = extract_value(line) if line.include? "$table_prefix"
+      end
+      wpconfig_to_hash(lines).merge( {"TABLE_PREFIX" => table_prefix} )
+    end
+
+    def wpconfig_to_hash(lines)
+      config = {}
+      lines.map do |setting|
+        setting = setting.split(",", 2)
+        setting.map! do |el|
+          format_value(el)
+        end
+        config[setting[0]] = setting[1]
+      end
+      config
+    end
+
+    def format_value(val)
+      val.strip!
+      val = true               if val.to_s.downcase.include? "true"
+      val = false              if val.to_s.downcase.include? "false"
+      val = val.to_i           if val.to_s.to_i.to_s == val
+      val = extract_value(val) if val.kind_of?(String) && !val.to_s.empty?
+      val
+    end
+
+    def extract_value(string)
+      string[/\'[^']*\'/].to_s.gsub(/\'/, "")
+    end
+
   end
 end
 
